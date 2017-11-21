5f16943d63d2dacd74eade48bf743eeaa5b405e8
Hamid Badiozamani <badiozam@yahoo.com>
2017-11-21 11:11:30 -0800

    Added import statements to numpy arrays so they can be used in Python.

--- catkin_ws/src/test_opencl/src/voxel_raytrace_gpu_module.cpp
+++ catkin_ws/src/test_opencl/src/voxel_raytrace_gpu_module.cpp
@@ -3,10 +3,20 @@
 BOOST_PYTHON_MODULE(_wrapper_test_opencl_module)
 {
     import_array();
+    numpy_boost_python_register_type<float, 1>();
+    numpy_boost_python_register_type<boost::uint8_t, 2>();
+    numpy_boost_python_register_type<boost::uint8_t, 1>();
+    numpy_boost_python_register_type<boost::int32_t, 1>();
+    numpy_boost_python_register_type<boost::int16_t, 1>();
+    numpy_boost_python_register_type<boost::int16_t, 2>();
+    numpy_boost_python_register_type<boost::uint32_t, 3>();
+    numpy_boost_python_register_type<boost::uint8_t, 3>();
+    numpy_boost_python_register_type<float, 3>();
+
+
     py::register_exception_translator<std::runtime_error>(&translate_exception);
     py::def("has_gpu_support", has_gpu_support, py::arg("debug"));
-    py::class_<VoxelRaytraceBoostWrapperGpu, boost::noncopyable>(
-        "TestOpenCLVoxelRaytraceWrapperGpu", boost::python::init<float, float, unsigned int, string const &>())
+    py::class_<VoxelRaytraceBoostWrapperGpu>("VoxelRaytraceBoostWrapperGpu", boost::python::init<float, float, unsigned int, string const &>())
             .def("raytrace_pointcloud", &VoxelRaytraceBoostWrapperGpu::raytrace_pointcloud)
             .def("print_debug_statistics", &VoxelRaytraceBoostWrapperGpu::print_debug_statistics)
     ;
